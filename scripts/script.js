6f6f5aed50427c326e972db60ddbc0128fd90d30
Antonio Souza <129562096+antoniosouza977@users.noreply.github.com>
2023-05-08 15:53:59 -0400

    Funcionalidade de criação de listas personalizadas

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+/* Criação de listas personalizadas */
+
+const tsk_li_sel = document.querySelector('#tasks-lists-sel')
+const add_new_list_btn = document.querySelector('#add-new-task-list')
+const new_tsk_itens = document.querySelectorAll('.ntsk-item')
+const cancel_newTask_btn = document.querySelector('#cancel-new-task-btn')
+const confirm_newTaskli_btn = document.querySelector('#confirm-list-btn')
+const name_newTaskLi = document.querySelector('#add-new-list-input')
+
+
+
+add_new_list_btn.addEventListener('click', () => {
+    add_new_list_btn.style.display = 'none'
+    new_tsk_itens.forEach((tskiten) => {
+        tskiten.style.display = 'flex';
+    })
+});
+cancel_newTask_btn.addEventListener('click', () => {
+    add_new_list_btn.style.display = 'flex'
+    new_tsk_itens.forEach((tskiten) => {
+        tskiten.style.display = 'none';
+    })      
+
+});
+confirm_newTaskli_btn.addEventListener('click',() => {
+    if (name_newTaskLi.value.length > 0){
+
+        let newtask = document.createElement('option');
+
+        newtask.value = name_newTaskLi.value;
+        newtask.textContent = name_newTaskLi.value;
+
+        tsk_li_sel.appendChild(newtask);
+
+        name_newTaskLi.value = '';
+        add_new_list_btn.style.display = 'flex'
+        new_tsk_itens.forEach((tskiten) => {
+        tskiten.style.display = 'none';
+        tsk_li_sel.value = newtask.value;
+    })      
+    } else{
+        name_newTaskLi.value = 'Digite um nome!'
+        setTimeout(() => {
+            name_newTaskLi.value = '';
+        },750)
+    }
+});
