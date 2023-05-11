0004331391bfd95dfefe1bee68eccd3d9b086031
Antonio Souza <129562096+antoniosouza977@users.noreply.github.com>
2023-05-10 20:36:09 -0400

    att

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// Selecionando HTML do documento para LocalStorage
+const Doc_HTML = document.querySelector('#main-box')
 
 /* Criação de listas personalizadas */
 let tasks = document.querySelectorAll('.task')
@@ -11,18 +13,23 @@ const currentList_h4 = document.querySelector('#current-list-h4')
 
 
 tasksLists_select.addEventListener('input', () => {
+    localStorage.setItem('listvalue',tasksLists_select.value) 
     new_task_sel.value = tasksLists_select.value;
     refresh_lists();
     toggleNewTaskDivs();
 })
 add_NewList_btn.addEventListener('click', () => {
     toggleNewTaskDivs();
+    closeSearch();
     currentList_h4.style.display = 'none';
     tasksLists_select.style.display = 'none';
     add_NewList_btn.style.display = 'none'
     new_tsk_itens.forEach((tskiten) => {
         tskiten.style.display = 'flex';
     })
+    name_newTaskLi.value = '';
+    
+    name_newTaskLi.focus();
 });
 cancel_NewList.addEventListener('click', () => {
    toggleNewListsDivs();      
@@ -41,13 +48,13 @@ confirm_newTaskli_btn.addEventListener('click',() => {
         currentList_h4.style.display = 'flex';
         tasksLists_select.style.display = 'flex';
         add_NewList_btn.style.display = 'flex'
-        new_task_sel.innerHTML = tasksLists_select.innerHTML;
         new_tsk_itens.forEach((tskiten) => {
         tskiten.style.display = 'none';
         tasksLists_select.value = newtask.value;
         refresh_lists();
-
-        
+        storageTasks();
+        new_task_sel.innerHTML = tasksLists_select.innerHTML;
+        localStorage.setItem('listvalue',tasksLists_select.value)
     })      
     } else{
         name_newTaskLi.value = 'Digite um nome!'
@@ -56,7 +63,50 @@ confirm_newTaskli_btn.addEventListener('click',() => {
         },750)
     }
 });
+/* Busca e filtro */
+const search_input = document.querySelector('#search-input')
+const search_btn = document.querySelector('#search-button')
+const back_search = document.querySelector('#erase_search')
+let tasks_titles = document.querySelectorAll('.taskTitle')
+const search_div = document.querySelector('#search_div')
 
+search_btn.addEventListener('click', () => {
+    toggleFIlterDiv();
+    toggleNewTaskDivs();
+    toggleNewListsDivs();
+    erase_search.style.display = 'flex'  
+    search_input.style.display = 'flex'
+    search_btn.style.display = 'none'
+    search_input.value = '';
+    search_input.focus();     
+})
+search_input.addEventListener('input', () => {
+    tasks_titles = document.querySelectorAll('.taskTitle')
+    let search_input_value = search_input.value.toLowerCase();
+    
+    tasks_titles.forEach(title => {
+        let titletext = title.textContent.toLowerCase();
+        if(titletext.includes(search_input_value)){
+            title.closest('div').style.display = 'flex'
+        } else {
+            title.closest('div').style.display = 'none' 
+        }
+    });
+});
+back_search.addEventListener('click', () => {
+    closeSearch();
+    FilterDiv.style.display = 'flex'
+    search_div.style.width= '100%'
+});
+
+//filtro
+
+const filter = document.querySelector('#tasks-filter')
+
+filter.addEventListener('change', () => {
+    refresh_filter();
+    
+  });
 /* Criação de novas tarefas */
 
 const new_task_btn = document.querySelector("#new-task-btn");
@@ -74,7 +124,9 @@ new_task_btn.addEventListener('click', () => {
     add_newTask_div.style.display = 'none'
     create_newTask_input.style.display = 'flex'
     new_task_sel.value = tasksLists_select.value;
-
+    input_newTask_Name.focus();
+    closeSearch();
+    refresh_lists();
 });
 confirm_newTask_btn.addEventListener('click', () => {
     if (!/^\s*$/.test(input_newTask_Name.value)) { // Testa se o valor do input não possui apenas espaços.
@@ -113,8 +165,10 @@ confirm_newTask_btn.addEventListener('click', () => {
         add_newTask_div.style.display = 'flex'
         create_newTask_input.style.display = 'none'
         tasks = document.querySelectorAll('.task')
+        tasks_titles = document.querySelectorAll('.taskTitle')
         refresh_lists();
         checkFirstTask();
+        storageTasks();
     } else {
         input_newTask_Name.value = 'Insira um nome!'
         setTimeout(() => {
@@ -159,7 +213,7 @@ document.addEventListener('click', (e) => {
 
     if(targetEl.classList.contains('remove-task')){
         parentEl.remove();
-        //storageTasks();
+        storageTasks();
         tasks = document.querySelectorAll('.task')
     }
 
@@ -182,20 +236,31 @@ document.addEventListener('click', (e) => {
             edit_task_div.style.display = 'none';
             tasks_mainBox.style.display = 'block';
         }
+        storageTasks();
     }
 });
 
 /* Funçoes de uso geral */
 
 const refresh_lists = () => {
-    let refreshToSelect = tasksLists_select.value.replace(/\s/g, '-')
-    tasks.forEach((task) => {
-        if (!task.classList.contains(`${refreshToSelect}`)){
-            task.style.display = 'none';
-        } else{
-            task.style.display = 'flex';
-        }
-    })
+    if (tasksLists_select.value.includes(' ')) {
+        let refreshToSelect = tasksLists_select.value.replace(/\s/g, '-')
+        tasks.forEach((task) => {
+            if (!task.classList.contains(`${refreshToSelect}`)){
+                task.style.display = 'none';
+            } else if(task.classList.contains(`${refreshToSelect}`)){
+                task.style.display = 'flex';
+            }
+        })
+    } else {
+        tasks.forEach((task) => {
+            if (!task.classList.contains(`${tasksLists_select.value}`)){
+                task.style.display = 'none';
+            } else if(task.classList.contains(`${tasksLists_select.value}`)){
+                task.style.display = 'flex';
+            }
+        })
+    }
 }
 
 const toggleNewTaskDivs = () => {
@@ -221,3 +286,74 @@ const checkFirstTask = () => {
         }
     }
 }
+const closeSearch = () => {
+    back_search.style.display = 'none'  
+    search_input.style.display = 'none'
+    search_btn.style.display = 'flex'
+    search_input.value = '';
+    tasks.forEach(task => {
+        task.style.display = 'flex'
+    });  
+}
+const FilterDiv = document.querySelector('#filter_div')
+const toggleFIlterDiv = () => {   
+    FilterDiv.style.display = 'none'
+}
+
+const storageTasks = () => {
+    localStorage.setItem('tasks', tasks_mainBox.innerHTML)
+    localStorage.setItem('lists', tasksLists_select.innerHTML)
+}
+const refresh_filter = () => {
+    const selectedOption = filter.value;
+
+    tasks.forEach(task => {
+        task.style.display = 'none';
+    })
+    
+    if ((selectedOption === 'all')) {
+        for (let i = 0; i < tasks.length; i++){
+            tasks[i].style.display = 'flex' 
+        }
+        refresh_lists();
+    } else if (selectedOption === 'done') {
+        tasks.forEach((task) => {
+            task.style.display = 'flex';
+        })
+        refresh_lists();
+        for (let i = 0; i < tasks.length; i++){
+            if(!tasks[i].classList.contains('done')){
+                tasks[i].style.display = 'none'
+            }
+        }
+    } else if (selectedOption === 'todo') {
+        tasks.forEach((task) => {
+            task.style.display = 'flex';
+        })
+        refresh_lists();
+        for (let i = 0; i < tasks.length; i++){
+            if(tasks[i].classList.contains('done')){
+                tasks[i].style.display = 'none'
+            }
+        }
+    } 
+}
+
+
+ document.addEventListener('DOMContentLoaded', () =>{
+    
+     if((localStorage.getItem('tasks') === null) && (localStorage.getItem('lists') === null)){
+        localStorage.setItem('lists', tasksLists_select.innerHTML)
+        localStorage.setItem('tasks', tasks_mainBox.innerHTML)
+    } else {
+        tasksLists_select.innerHTML = localStorage.getItem('lists')
+        tasks_mainBox.innerHTML = localStorage.getItem('tasks')
+        new_task_sel.innerHTML = tasksLists_select.innerHTML;
+    } 
+    if (localStorage.getItem('listvalue') != null) {
+        tasksLists_select.value = localStorage.getItem('listvalue')
+    }
+    refresh_lists();
+    
+})
+ 
